76d0e35e701d564cab806069e4592add7d3ada5c
guevara1981 <andresguevara_1981@hotmail.com>
2022-08-20 10:57:37 -0500

    comentario en cada archivo

--- resta.py
+++ resta.py
@@ -1,3 +1,4 @@
+# Este codigo permite restar dos numeros ingresados por el usuario
 num1 = int (input("Ingrese El Numero Uno: "))
 num2 = int (input("Ingrese El Numero Dos: "))
 resultado = int (num1-num2)
--- suma.py
+++ suma.py
@@ -1,3 +1,4 @@
+# Este codigo permite sumar dos numeros ingresados por el usuario
 num1 = int (input("Ingrese El Numero Uno: "))
 num2 = int (input("Ingrese El Numero Dos: "))
 resultado = int (num1+num2)
