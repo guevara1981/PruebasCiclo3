b368a1a8ac38b233b9102b1835d9e97efbcebff2
guevara1981 <andresguevara_1981@hotmail.com>
2022-08-20 10:09:21 -0500

    Se modifico nombre de variable en linea print de r a resultado

--- suma.py
+++ suma.py
@@ -1,4 +1,4 @@
 a = int (input("Ingrese El Numero Uno: "))
 b = int (input("Ingrese El Numero Dos: "))
 resultado = int (a+b)
-print (f"El Resultado de la Suma es: {r}")
+print (f"El Resultado de la Suma es: {resultado}")
