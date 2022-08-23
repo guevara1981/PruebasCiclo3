37d18ffd11b6a5481340f84fa2e8a19b6754caf9
ANDRES GUEVARA <andresguevara_1981@hotmail.com>
2022-08-22 20:54:19 -0500

    Delete suma.py

--- suma.py
+++
@@ -1,5 +0,0 @@
-# Este codigo permite sumar dos numeros ingresados por el usuario
-num1 = int (input("Ingrese El Numero Uno: "))
-num2 = int (input("Ingrese El Numero Dos: "))
-resultado = int (num1+num2)
-print (f"El Resultado de la Suma es: {resultado}")
